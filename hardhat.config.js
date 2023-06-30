1cbb5f33d21738786f9d6d03a1f8c4d36ffc3dc4
RAKESH UMMADI <104605032+RAKESH9494@users.noreply.github.com>
2023-06-30 11:31:15 +0530

    first_commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,15 +2,15 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
-const { AURORA_TESTNET_PRIVATE_KEY } = process.env;
+const RSK_TESTNET_PRIVATE_KEY = process.env.RSK_TESTNET_PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.1",
  networks: {
    hardhat: {},
-   aurora_testnet: {
-    url: 'https://aurora-testnet.infura.io/v3/8c698cf7a21a45df9dbe0cfa11a3d905',
-    accounts: [`0xd180f1781f91a5daeea1714f43cbb722bb39b14872affa4fb01d0bba36e628ff`],
-    chainId: 1313161555
+   rsk_testnet: {
+    url: "https://public-node.testnet.rsk.co",
+    accounts: [`0x8c14811e4530e2e8293f158e1883538409269a44709cf8db64e11858cca73737`],
+    chainId: 31
   }
  },
 };
